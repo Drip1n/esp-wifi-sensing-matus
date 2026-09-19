a4e3f13695b9897441b50bfa8d57ddca37d6a858
Drip1n <milansmiesko4@gmail.com>
2026-09-20 01:45:02 +0200

    komunikacia s laptopu do esp funguje a led svieti

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,43 @@
 #include <Arduino.h>
+#include <Adafruit_NeoPixel.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define LED_PIN 48
+#define LED_COUNT 1
+
+Adafruit_NeoPixel led(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+
+  led.begin();
+  led.clear();
+  led.show();
+
+  Serial.println("RGB LED test started!");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  // RED
+  led.setPixelColor(0, led.Color(255, 0, 0));
+  led.show();
+  Serial.println("RED");
+  delay(1000);
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  // GREEN
+  led.setPixelColor(0, led.Color(0, 255, 0));
+  led.show();
+  Serial.println("GREEN");
+  delay(1000);
+
+  // BLUE
+  led.setPixelColor(0, led.Color(0, 0, 255));
+  led.show();
+  Serial.println("BLUE");
+  delay(1000);
+
+  // OFF
+  led.clear();
+  led.show();
+  Serial.println("OFF");
+  delay(1000);
 }
